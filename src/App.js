4571eb1c9bb308c5832809143fdfd04edf0047b6
thiagoalmeida1905 <thi.almeida1905@gmail.com>
2023-09-12 19:40:42 -0300

    Iniciando projeto 'organo' da Alura

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Banner from './componentes/Banner/Banner';
+import Formulario from './componentes/Formulario';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <Formulario/>
+    </div>
+  );
+}
+
+export default App;
