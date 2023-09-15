d8dcd3455cfb8a2a596f8c15729256b75f97ab9d
thiagoalmeida1905 <thi.almeida1905@gmail.com>
2023-09-14 21:18:23 -0300

    cadastro dos colaborador

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,11 +1,20 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
+    //let valor = ''
+
+
+
+    const aoDigitado = (evento) => {
+       props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,12 +1,13 @@
 import './ListaSuspensa.css'
+import {useState} from 'react';
 
 const ListaSuspensa = (props) => {
 
     return (
         <div className="lista-suspensa">
             <label>{props.label}</label>
-            <select required={props.required}> 
-                {props.itens.map(item => <option key={item}>{item}</option>)}
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.value}> 
+                {props.itens.map(item => { return <option key={item}>{item}</option>})}
             </select>
         </div>
     )
